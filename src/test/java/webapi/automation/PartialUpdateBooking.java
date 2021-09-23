d9cf53b5bfc11c6148730ed0696cf35ada9a22f3
ugurulas <60687856+ugurulas@users.noreply.github.com>
2021-09-23 19:36:00 +0300

    Update PartialUpdateBooking.java

--- src/test/java/webapi/automation/PartialUpdateBooking.java
+++ src/test/java/webapi/automation/PartialUpdateBooking.java
@@ -1,27 +1,56 @@
 package webapi.automation;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import utilities.HelperMethods;
 
-import com.squareup.okhttp.*;
-import com.sun.org.glassfish.gmbal.Description;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 import java.io.IOException;
-@Test(description="When we deal with PartialUpdateBooking, we should use PATCH Method")
-public class PartialUpdateBooking extends BaseClass{
+ @Test
+    public void updateBookingTests() {
 
-    OkHttpClient client = new OkHttpClient();
-    MediaType mediaType = MediaType.parse("application/json");
-    RequestBody body = RequestBody.create(mediaType, "{\r\n    \"firstname\" : \"ugurtest\",\r\n    \"lastname\" : \"Brown\"\r\n}");
-    Request request = new Request.Builder()
-            .url(BASE_ENDPOINT+"/booking/13")
-            .method("PATCH", body)
-            .addHeader("Content-Type", "application/json")
-            .addHeader("Accept", "application/json")
-            .addHeader("Cookie", TOKEN)
-            .build();
-    Response response = client.newCall(request).execute();
+        // Create JSON body
+        Response responseCreate = createBooking();
+        responseCreate.prettyPrint();
 
-    public PartialUpdateBooking() throws IOException {
+        JSONObject body = new JSONObject();
+        body.put("firstname", "Irina");
+
+        JSONObject bookingdates = new JSONObject();
+        bookingdates.put("checkin", "2021-06-01");
+        body.put("bookingdates", bookingdates);
+
+        // get booking id of the new booking
+        int bookingId = responseCreate.jsonPath().getInt("bookingid");
+
+        // Get the response using booking ID and authentication credentials
+        Response responseUpdate = RestAssured.given(rs)
+                .auth().preemptive().basic("admin", "password123")
+                .contentType(ContentType.JSON).body(body.toString())
+                .patch("/booking/" + bookingId);
+
+        // Verify All fields
+        int expected = 200;
+        int actual = responseUpdate.getStatusCode();
+
+        Assertions.assertEquals(expected, actual, "Status code is supposed to be 200, but it is " + actual);
+
+        System.out.println("The actual status code is " + actual + ", and it's supposed to be " + expected + ".");
+
+        String actualFirstName = responseUpdate.jsonPath().getString("firstname");
+        String expectedFirstName = "Irina";
+        Assertions.assertEquals(expectedFirstName, actualFirstName, "The name in the response is "
+                + actualFirstName + ", and it's supposed to be " + expectedFirstName);
+
+        String actualCheckIn = responseUpdate.jsonPath().getString("bookingdates.checkin");
+        String expectedCheckIn = "2021-06-01";
+        Assertions.assertEquals(expectedCheckIn, actualCheckIn, "checkin in response is not expected");
+
+
+        responseUpdate.prettyPrint();
     }
 }

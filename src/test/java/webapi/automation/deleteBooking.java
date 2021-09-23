812294d7203be936c1239b0214ec1a4cfca599b0
ugurulas <60687856+ugurulas@users.noreply.github.com>
2021-09-23 19:33:32 +0300

    Update deleteBooking.java

--- src/test/java/webapi/automation/deleteBooking.java
+++ src/test/java/webapi/automation/deleteBooking.java
@@ -8,14 +8,27 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class deleteBooking extends BaseClass {
-    @Test
-    public void deleteIsSuccessful() throws IOException {
+@Test
+    public void deleteBookingTests() {
 
-        HttpDelete request = new HttpDelete(BASE_ENDPOINT);
+        // Create JSON body
+        Response responseCreate = createBooking();
+        responseCreate.prettyPrint();
 
-        request.setHeader(HttpHeaders.AUTHORIZATION, "token " + Credentials.TOKEN);
-        response = client.execute(request);
-        int actualStatusCode = response.getStatusLine().getStatusCode();
-        Assert.assertEquals(actualStatusCode, 204);
-    }
+        // Get bookingId of new booking
+        int bookingid = responseCreate.jsonPath().getInt("bookingid");
+
+        // Delete booking
+        Response responseDelete = RestAssured.given(rs).auth().preemptive().basic("admin", "password123")
+                .delete("/booking/" + bookingid);
+        responseDelete.print();
+
+        // Verifications
+        // Verify response 201
+        Assertions.assertEquals(201, responseDelete.getStatusCode(),  "Status code should be 201, but it's not.");
+
+        Response responseGet = RestAssured.get("https://restful-booker.herokuapp.com/booking/" + bookingid);
+        responseGet.print();
+
+        Assertions.assertEquals("Not Found",responseGet.getBody().asString(),  "Body should be 'Not Found', but it's not.");
 }
